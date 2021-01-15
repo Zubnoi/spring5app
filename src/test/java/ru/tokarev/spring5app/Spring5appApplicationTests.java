16d5081c9f5ab7cc61267b24db4653343e3efb0c
Andrey Tokarev <tokarevandy@gmail.com>
2021-01-15 23:11:09 +0300

    Initial commit

---
+++ src/test/java/ru/tokarev/spring5app/Spring5appApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tokarev.spring5app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5appApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
