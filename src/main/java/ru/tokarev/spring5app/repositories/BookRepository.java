5a098e8a2bc799a8f414b7b50fa71b85b9056aa7
Andrey Tokarev <tokarevandy@gmail.com>
2021-01-16 14:35:25 +0300

    third commit

--- src/main/java/ru/tokarev/spring5app/repositories/BookRepository.java
+++ src/main/java/ru/tokarev/spring5app/repositories/BookRepository.java
@@ -3,6 +3,11 @@ package ru.tokarev.spring5app.repositories;
 import org.springframework.data.repository.CrudRepository;
 import ru.tokarev.spring5app.domain.Book;
 
+/**
+ * @author tokar on 16.01.2021
+ * @project spring5app
+ */
+
 
 public interface BookRepository extends CrudRepository<Book, Long> {
 }
